9c97fe2fc242d62ba822ed53235538aeba40de0a
Jimwell <75101022+ebonchristian@users.noreply.github.com>
2023-07-12 22:29:24 +0800

    Commit changes.

---
+++ VitalSignsApp/src/app/not-normal/not-normal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-normal',
+  templateUrl: './not-normal.page.html',
+  styleUrls: ['./not-normal.page.scss'],
+})
+export class NotNormalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
